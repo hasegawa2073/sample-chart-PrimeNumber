d485f2da83b9477574f5c973d51460702da624b2
hasegawa2073 <hasetatsu2001@icloud.com>
2022-08-24 22:06:08 +0900

    Add: 入力した数字の列数で表示できるようにした

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const tbody = document.querySelector('.tbody');
+  const table = document.querySelector('.table');
 
   // 素数のときtrueを返す関数
   const primeJudge = (num) => {
@@ -19,12 +19,50 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   };
 
-  const tdArray = [];
-  const newTdArray = [];
   const max = 1000;
   let column = 10;
 
-  const chartUpdate = () => {
+  const deleteChart = () => {
+    const tr = document.querySelectorAll('.tr');
+    tr.forEach((tr) => {
+      tr.remove();
+    });
+  };
+
+  const initChart = () => {
+    const tdArray = [];
+    const newTdArray = [];
+    const newTbody = document.createElement('tbody');
+    newTbody.classList.add('tbody');
+    for (let i = 1; i <= max; i++) {
+      const td = document.createElement('td');
+      td.textContent = i;
+      if (primeJudge(i)) {
+        td.classList.add('primeNumber');
+      }
+      tdArray.push(td);
+      // 行単位でtdをnewTdArrayへ
+      if (tdArray.length % column === 0) {
+        const row = tdArray.slice(i - column, i + column);
+        newTdArray.push(row);
+      }
+    }
+    newTdArray.forEach((row) => {
+      const tr = document.createElement('tr');
+      tr.classList.add('tr');
+      for (let i = 0; i < row.length; i++) {
+        tr.appendChild(row[i]);
+      }
+      newTbody.appendChild(tr);
+      table.appendChild(newTbody);
+    });
+  };
+  initChart();
+
+  const createChart = () => {
+    const tdArray = [];
+    const newTdArray = [];
+    const tbody = document.querySelector('.tbody');
     for (let i = 1; i <= max; i++) {
       const td = document.createElement('td');
       td.textContent = i;
@@ -48,11 +86,10 @@ document.addEventListener('DOMContentLoaded', function () {
     });
   };
 
-  chartUpdate();
-
   const inputColumn = document.querySelector('#column');
-  inputColumn.addEventListener('input', function (e) {
+  inputColumn.addEventListener('change', function (e) {
     column = e.target.value;
-    console.log(e.target.value);
+    deleteChart();
+    createChart();
   });
 });
